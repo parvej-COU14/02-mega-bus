57f59c9396861cf728f559725db4f9b1e1b11bcd
parvej mosharaf <parvejmosharaf62462@gmail.com>
2021-07-01 00:51:22 +0600

    modify the project

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-// First -Class part
+// First,Economic both-part  -Class part
+// Way-01 solution
+
+function InputFistCase(isIncrease,isDecrease,id){
+    
+    const firstCase=document.getElementById("id");
+    const firstCaseValue=parseInt(firstCase.value);
+    let firstCaseValueNew=firstCaseValue;
+    if(isIncrease==true && isDecrease==true){
+        firstCaseValueNew=firstCaseValue+1;
+    }
+    if(isIncrease==false&&isDecrease==false&&firstCaseValue>0){
+        firstCaseValueNew=firstCaseValue-1;
+    }
+   
+    firstCase.value=firstCaseValueNew;
+    TotalAmount();
+}
+
+function TotalAmount(){
+    const firstClass=document.getElementById("valueBtn");
+    const firstClassValue=parseInt(firstClass.value);
+
+    const firstClassEconomic=document.getElementById("valueBtnEconomic");
+    const economicCaseValue=parseInt(firstClassEconomic.value);
+
+    const subTotal=firstClassValue*150+economicCaseValue*100;
+    document.getElementById("subTotal").innerText=subTotal;
+    const taxAmount=subTotal*0.1;
+    document.getElementById("tax").innerText=taxAmount;
+    const total=subTotal-taxAmount;
+    document.getElementById("total").innerText=total;
+
+}
+
+ // booking  -Class part
+ 
+const hideBtn=document.getElementById("bookBtn");
+hideBtn.addEventListener('click',function(){
+    // const bookhide=document.getElementById("bookForm");
+    // bookhide.style.display="none";
+    alert("Congratulations for your booking")
+   
+})
+
+// Way-02 solution
 
 function InputFistCase(isIncrease){
     const firstCase=document.getElementById("valueBtn");
@@ -30,6 +75,7 @@ function InputEconomicCase(isDecrease){
     firstCase.value=firstCaseValueNew;
     TotalAmount();
 }
+
 // Calculation  -Class part
 
 function TotalAmount(){
